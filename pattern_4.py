c58b2f3c6c363d48fefd2d09b6dd332df52b97b7
Rohit Luni <87267331+rohit4242@users.noreply.github.com>
2021-08-15 18:00:47 +0530

    Add files via upload

---
+++ pattern_4.py
@@ -0,0 +1,19 @@
+'''
+
+* * * * * * * 
+  * * * * * * 
+    * * * * * 
+      * * * * 
+        * * * 
+          * * 
+            * 
+
+'''
+
+n = int(input('Enter the your row: '))
+for row in range(n, 0, -1):
+    for col in range(0, n-row):
+        print(' ', end=' ')
+    for k in range(1, row+1):
+        print('*', end=' ')
+    print()
